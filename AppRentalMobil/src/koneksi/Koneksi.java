dbcc7f58124c358f899fc907855a7490ef78d9db
NineOne-Code <uk.name2017@gmail.com>
2020-07-13 00:15:53 +0700

    Initial commit

---
+++ AppRentalMobil/src/koneksi/Koneksi.java
@@ -0,0 +1,38 @@
+package koneksi;
+
+
+import java.sql.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nineone
+ */
+public class Koneksi {
+    private Connection koneksi;
+    public Statement stm;
+    String url = "jdbc:mysql://localhost/rental_mobil";
+    
+    public Connection connect() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("berhasil connect");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("gagal connect" + ex);
+        }
+        
+        try {
+            koneksi = DriverManager.getConnection(url, "root", "");
+            stm = koneksi.createStatement();
+            System.out.println("berhasil connect database");
+        } catch (SQLException ex) {
+            System.out.println("gagal connect database" + ex);
+        }
+        return koneksi;
+    }
+}
